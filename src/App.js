2dab0a521bc71e4b305a9d5f0dc03d460b864c79
klevessonroseno <klevesson.developer@gmail.com>
2022-10-26 15:29:44 -0300

    Creating class App

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use();
+  }
+}
+
+new App().server.listen(3333);
