52b76964edaf5bc2dd6c3cba29498f5c7e7f3bf5
klevessonroseno <klevesson.developer@gmail.com>
2022-10-27 10:30:13 -0300

    Listing all providers

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import UserController from './app/controllers/UserController';
 import authMiddleware from './app/middlewares/auth';
 
 import multerConfig from './config/multer';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = Router();
 const upload = multer(multerConfig);
@@ -18,6 +19,8 @@ routes.post('/users', UserController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
