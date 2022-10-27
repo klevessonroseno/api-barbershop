0c23c8c6bf1d892e916e79faf181de375ad10f23
klevessonroseno <klevesson.developer@gmail.com>
2022-10-27 11:04:08 -0300

    Creating Appointment model

---
+++ src/app/models/Appointment.js
@@ -0,0 +1,31 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Appointment extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        date: Sequelize.DATE,
+        path: Sequelize.DATE,
+      }, 
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, {
+      foreignKey: 'user_id',
+      as: 'user',
+    });
+
+    this.belongsTo(models.User, {
+      foreignKey: 'provider_id',
+      as: 'provider',
+    });
+  }
+}
+
+export default Appointment;
