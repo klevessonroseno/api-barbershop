b0a430e0e1e98fd47f9bae00cc482fb2c0789575
klevessonroseno <klevesson.developer@gmail.com>
2022-10-27 15:47:52 -0300

    Listing schedule provider

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = Router();
 const upload = multer(multerConfig);
@@ -26,6 +27,8 @@ routes.get('/providers', ProviderController.index);
 routes.post('/appointments', AppointmentController.store);
 
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index)
 
 routes.post('/files', upload.single('file'), FileController.store);
 
