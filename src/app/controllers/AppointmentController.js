83b6c939e4538874c6b1f2301d3757049493874f
klevessonroseno <klevesson.developer@gmail.com>
2022-10-28 18:29:42 -0300

    Configuring queue with redis.

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,7 +6,8 @@ import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
 
-import Mail from '../../lib/Mail';
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail';
 
 class AppointmentController {
   async store(req, res) {
@@ -160,22 +161,12 @@ class AppointmentController {
 
     await appointment.save();
 
-    const [providerFirstName] = appointment.provider.name.split(' ');
-
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado.', 
-      template: 'cancellation',
-      context: {
-        provider: providerFirstName,
-        user: appointment.user.name,
-        date: format(
-          appointment.date,
-          "'dia' dd 'de' MMMM', às' H:mm'h'",
-          { locale: pt },
-        ),
+    await Queue.add(
+      CancellationMail.key, 
+      {
+        appointment,
       }
-    });
+    );
 
     return res.status(200).json(appointment);
   }
