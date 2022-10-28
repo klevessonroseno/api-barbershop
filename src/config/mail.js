6aac3b68f61ec22ae9bb9f9e4007c7a7f16b42de
klevessonroseno <klevesson.developer@gmail.com>
2022-10-28 14:05:29 -0300

    Sending an email to the provider when a user cancels an appointment.

---
+++ src/config/mail.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+
+export default {
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+  default: {
+    from: 'Equipe Barbershop <noreply@barbershop.com>',
+  }
+}
